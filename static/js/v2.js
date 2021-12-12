34419f3b07386ef3d8df2e662afadcda36ff13de
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2021-12-12 12:10:12 +0100

    Refonte du code js, HTML et adaptation du css. [Part. 2]
    HTML (section du meilleur film) :
        - Début de la refonte.
    
    JS :
        - Le choix des catégories se fait dans v2.js à la fonction choosingCategories().
        - Titre des films en info-bulles => titre original.
        - Gestion catégorie des meilleurs films (s'affiche si choist). Cette catégorie exclura toujrous LE meilleur film (qui a son emplacement réservés).
        - Le meilleur film sera toujours affiché, même si l'on ne choisit pas pour l'une des trois catégories, celle des meilleurs films.
        - Préparation pour chargement des data du meilleur film.
    
    CSS .
        - Correction pour la section réservé au meilleur film.
    
    BUG :
        - en JS, si une catégorie est choisit plus d'une fois, alors seul sa première itération dans la liste donnera un résultats (les autres ne conduiront pas à un chargement de code HTML dans la page).

--- static/js/v2.js
+++ static/js/v2.js
@@ -1,17 +1,27 @@
 // 0. Définir les adresses des URLS
+async function choosingCategories() {
+    const genres = ["", "biography", "sci-fi", "adventure"]; // maximun 4 genres. Idéalement 4.
+    for (const genre of genres) {
+        const countCategory = genres.indexOf(genre);
+        // console.log(genre, countCategory)
+        await urlsCategory(genre, countCategory);
+    };
+};
+
+
 async function urlsCategory(genre, countCategory) {
     for (let i = 1; i <= 2; i++) {
         const url = `http://localhost:8000/api/v1/titles/?genre=${genre}` + 
                     `&page=${i}&sort_by=-imdb_score%2C-votes`;
         const countPage = i;
-        await loadData(url, countPage, genre, countCategory);
+        await loadData(url, genre, countCategory, countPage);
     };
 };
 
 
 // 1. Télécharger les informations des films depuis le serveur
 
-async function loadData(url, countPage, genre, countCategory) {
+async function loadData(url, genre, countCategory, countPage) {
     await fetch(url, {
         method: "GET",
         headers: {
@@ -20,60 +30,62 @@ async function loadData(url, countPage, genre, countCategory) {
         }
     })
         .then(res => res.json())
-        .then(json => displayData(json, countPage, genre, countCategory))
-        .catch(err => console.log(err))
+        .then(json => displayData(json, genre, countCategory, countPage))
+        .catch(err => console.log(err));
 };
 
 
 // 2. Créer le blocs d'éléments Html pour chaque film à afficher
 
-function displayData(json, countPage, genre, countCategory) {
-    if (countPage === 1) {
-        var countMovie = 4;
-        displayNameCategory(genre, countCategory);
-        displayPosterMovies(json, genre, countMovie, countCategory);
-    };
-    if (countPage === 2) {
-        var countMovie = 1;
-        displayPosterMovies(json, genre, countMovie, countCategory);
+async function displayData(json, genre, countCategory, countPage) {
+    // Serait intéréssant d'ajouter une fonction ici (avant le remplissage) 
+    // qui ajouterait le code HTML de la structure des catégories. 
+    // On aurait alors autant que catégories que renseignées dans choosingCategories().
+
+
+    if (countCategory === "00") { // CountCategory = "00" correspond à la section meilleur film.
+        if (countPage === 1) {
+            displayBestMovie(json);
+        };
+    }else {
+        if (countPage === 1) {
+            if (genre === "") {
+                var idxStart = 1;
+            }else {
+                var idxStart = 0;
+            };
+            var countMovie = 4;
+            displayNameCategory(genre, countCategory);
+            displayPosterMovies(json, countCategory, idxStart, countMovie);
+        };
+        if (countPage === 2) {
+            var idxStart = 0;
+            if (genre === "") {
+                var countMovie = 2;
+            }else {
+                var countMovie = 1;
+            };
+            displayPosterMovies(json, countCategory, idxStart, countMovie);
+        };
     };
 };
 
+function displayBestMovie(json) {
+    document.getElementById("title_best_movie").innerHTML = json.results[0].title
+    document.getElementById("synopsis_best_movie").innerHTML = "Seulement pour tester"
+    document.getElementById("poster_best_movie").setAttribute("src", json.results[0].image_url)
+    document.getElementById("poster_best_movie").setAttribute("alt", `poster of ${json.results[0].title}`)
+};
+
 function displayNameCategory(genre, countCategory) {
-    let nameCategory = document.getElementById(`category${countCategory}`);
-    // let translation = {
-    //     "action": "d'action",
-    //     "adult": "pour adulte",
-    //     "adventure": "d'aventure",
-    //     "animation": "d'animation",
-    //     "biography": "biopic",
-    //     "comedy": "de comédie",
-    //     "crime": "policier",        //
-    //     "documentary": "documentaire",
-    //     "drama": "de drame",
-    //     "family": "familliaux",
-    //     "fantasy": "fantastique",
-    //     "film-noir": "en noir & blanc",
-    //     "history": "historique",
-    //     "horor": "d'épouvante-horreur",
-    //     "music": "de comédie musical",
-    //     "musical": "musical",
-    //     "mystery": "policier",       //
-    //     "news": "nouveauté",
-    //     "reality-TV": "de télé-réalité",
-    //     "romance": "de romance",
-    //     "sci-fi": "de science-fiction",
-    //     "sport": "sur le sport",
-    //     "thriller": "thriller",
-    //     "war": "de guerre",
-    //     "western": "western"
-    // };
+    let nameCategory = document.getElementById(`title_category${countCategory}`);
     let translation = translateNameCategory(genre);
     nameCategory.innerHTML = `Films ${translation}`
 };
 
 function translateNameCategory(genre) {
     let translation = {
+        "": "les mieux notés",
         "action": "d'action",
         "adult": "pour adulte",
         "adventure": "d'aventure",
@@ -103,20 +115,37 @@ function translateNameCategory(genre) {
     return translation[genre];
 };
 
-function displayPosterMovies(json, genre, countMovie, countCategory) {
+async function displayPosterMovies(json, countCategory, idxStart, countMovie) {
     let categoryContainer = document.getElementById(`best_category${countCategory}_movies`);
-    for (let i = 0; i <= countMovie; i++) {
+    for (let i = idxStart; i <= countMovie; i++) {
         let film = json.results[i];
+        let originalTitle = await getOriginalTitle(film.id);
         categoryContainer.innerHTML += 
                 `
                 <button id="${film.id}" class="movie_container" 
-                title="${film.title}">
+                title="${originalTitle}">
                     <img class="movie_poster_container" src="${film.image_url}"
-                    alt="poster_of_${film.title}"/>
+                    alt="poster of «${originalTitle}»" />
                 </button>
                 `
     };
         //document.getElementById(`${film.id}`).addEventListener('Click', displayModal(`${film.id}`));
+};
+
+
+async function getOriginalTitle(idMovie) {
+    var originalTitle;
+    await fetch(`http://localhost:8000/api/v1/titles/${idMovie}`, {
+        method: "GET",
+        header: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(json => {originalTitle = json.original_title;})
+    .catch(err => console.log(err));
+    return originalTitle;
 };
 
 // 3. Ajouter le bloc Html du film dans la page Html
@@ -126,11 +155,8 @@ function displayPosterMovies(json, genre, countMovie, countCategory) {
 
 
 function main() {
-    const genres = ["biography", "sci-fi", "adventure"];
-    for (const genre of genres) {
-        const countCategory = genres.indexOf(genre) + 1;
-        urlsCategory(genre, countCategory);
-    };
-};
+    urlsCategory("", "00");
+    choosingCategories();
+}
 
 document.addEventListener('DOMContentLoaded', main());
